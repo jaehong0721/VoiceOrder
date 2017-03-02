5b0b3c84a183001aa03c897026ced81c8c1279d4
jaehong <newbie302@gmail.com>
2017-03-02 20:00:19 +0900

    set app theme and color change

--- app/src/main/java/com/rena21c/voiceorder/activities/MainActivity.java
+++ app/src/main/java/com/rena21c/voiceorder/activities/MainActivity.java
@@ -3,12 +3,16 @@ package com.rena21c.voiceorder.activities;
 import android.os.Bundle;
 
 import com.rena21c.voiceorder.R;
+import com.rena21c.voiceorder.view.actionbar.ActionBarViewModel;
 
 public class MainActivity extends BaseActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_main);
+
+        ActionBarViewModel.createWithActionBar(getSupportActionBar());
     }
 }
