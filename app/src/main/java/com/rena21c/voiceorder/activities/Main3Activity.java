403019bf2c6281cfef79616925eaa59681e4b9e3
jaehong <newbie302@gmail.com>
2017-05-30 15:10:56 +0900

    추천탭, 내거래처 탭에 해당하는 activity 추가

---
+++ app/src/main/java/com/rena21c/voiceorder/activities/Main3Activity.java
@@ -0,0 +1,16 @@
+package com.rena21c.voiceorder.activities;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.rena21c.voiceorder.R;
+
+
+public class Main3Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main3);
+    }
+}
