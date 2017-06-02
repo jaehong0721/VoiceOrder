f89e7f50f6884f7c2b4bf7aa3e7243333edaf7ae
jaehong <newbie302@gmail.com>
2017-06-02 21:14:28 +0900

    근처 납품업체 리스트 요청하는 서버 api 추가

--- app/src/main/java/com/rena21c/voiceorder/network/ApiService.java
+++ app/src/main/java/com/rena21c/voiceorder/network/ApiService.java
@@ -1,15 +1,23 @@
 package com.rena21c.voiceorder.network;
 
 
-import com.rena21c.voiceorder.activities.SplashActivity;
 import com.rena21c.voiceorder.pojo.UserToken;
+import com.rena21c.voiceorder.pojo.Vendor;
+
+import java.util.HashMap;
+import java.util.List;
 
 import retrofit2.Call;
+import retrofit2.http.Body;
 import retrofit2.http.GET;
+import retrofit2.http.POST;
 import retrofit2.http.Query;
 
 public interface ApiService {
 
     @GET("/requestTokenWithPhoneNumber")
     Call<UserToken> getToken(@Query("phoneNumber") String phoneNumber);
+
+    @POST("/vendor/getNearbyVendors")
+    Call<List<Vendor>> getNearbyVendors(@Body HashMap<String, Double> bodyMap);
 }
