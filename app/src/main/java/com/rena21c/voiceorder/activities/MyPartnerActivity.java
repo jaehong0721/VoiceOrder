e934e6fd3ad99086abf96588b6951c00d1f2baa0
jaehong <newbie302@gmail.com>
2017-06-12 19:24:51 +0900

    내연락처에서 추가 버튼 클릭시 AddPartnerActivity로 이동

--- app/src/main/java/com/rena21c/voiceorder/activities/MyPartnerActivity.java
+++ app/src/main/java/com/rena21c/voiceorder/activities/MyPartnerActivity.java
@@ -1,20 +1,26 @@
 package com.rena21c.voiceorder.activities;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 
 import com.rena21c.voiceorder.App;
 import com.rena21c.voiceorder.R;
 import com.rena21c.voiceorder.etc.AppPreferenceManager;
 import com.rena21c.voiceorder.view.components.ReplaceableLayout;
+import com.rena21c.voiceorder.view.widgets.AddPartnerButton;
+import com.rena21c.voiceorder.viewmodel.MyPartnerGuideViewModel;
 
 
-public class MyPartnerActivity extends HasTabActivity {
+public class MyPartnerActivity extends HasTabActivity implements AddPartnerButton.AddPartnerListener{
 
     private AppPreferenceManager appPreferenceManager;
 
+    private MyPartnerGuideViewModel guideViewModel;
+
     private ReplaceableLayout replaceableLayout;
-    private View myPartnerGuide;
+    private View myPartnerGuideView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,14 +28,31 @@ public class MyPartnerActivity extends HasTabActivity {
         setContentView(R.layout.activity_my_partner);
 
         replaceableLayout = (ReplaceableLayout)findViewById(R.id.replaceableLayout);
-        myPartnerGuide = getLayoutInflater().inflate(R.layout.layout_component_my_partner_guide, replaceableLayout, false);
 
         appPreferenceManager= App.getApplication(getApplicationContext()).getPreferenceManager();
+
+        guideViewModel = new MyPartnerGuideViewModel();
+        myPartnerGuideView = guideViewModel.getView(this);
+    }
+
+    @Override protected void onPause() {
+        super.onPause();
+        Log.d("test", "mypartneracitivity onpause");
+    }
+
+    @Override protected void onStop() {
+        super.onStop();
+        Log.d("test", "mypartneracitivity onstop");
     }
 
     @Override protected void onResume() {
         super.onResume();
         replaceableLayout.removeAllViews();
-        if(appPreferenceManager.getAllCallTime() == null) replaceableLayout.replaceChildView(myPartnerGuide);
+        if(appPreferenceManager.getAllCallTime() == null) replaceableLayout.replaceChildView(myPartnerGuideView);
+    }
+
+    @Override public void onAddPartner() {
+        Intent intent = new Intent(MyPartnerActivity.this, AddPartnerActivity.class);
+        startActivity(intent);
     }
 }
---
+++ app/src/main/java/com/rena21c/voiceorder/viewmodel/MyPartnerGuideViewModel.java
@@ -0,0 +1,21 @@
+package com.rena21c.voiceorder.viewmodel;
+
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+
+import com.rena21c.voiceorder.R;
+import com.rena21c.voiceorder.view.widgets.AddPartnerButton;
+
+public class MyPartnerGuideViewModel {
+
+    public View getView(Context context) {
+        View view = LayoutInflater.from(context).inflate(R.layout.layout_component_my_partner_guide, null);
+        AddPartnerButton addPartnerButton = (AddPartnerButton) view.findViewById(R.id.btnAddPartner);
+
+        addPartnerButton.setAddPartnerListener((AddPartnerButton.AddPartnerListener)context);
+
+        return view;
+    }
+}
