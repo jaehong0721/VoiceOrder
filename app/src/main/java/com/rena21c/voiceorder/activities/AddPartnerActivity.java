99b1455338c8dd8b11e40fd143042b5c8cfa8743
jaehong <newbie302@gmail.com>
2017-06-12 19:23:05 +0900

    AddPartnerActivity 추가

---
+++ app/src/main/java/com/rena21c/voiceorder/activities/AddPartnerActivity.java
@@ -0,0 +1,15 @@
+package com.rena21c.voiceorder.activities;
+
+import android.os.Bundle;
+
+import com.rena21c.voiceorder.R;
+
+public class AddPartnerActivity extends BaseActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_partner);
+        
+    }
+}
