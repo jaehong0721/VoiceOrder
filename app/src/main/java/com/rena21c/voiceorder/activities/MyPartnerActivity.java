99652815b0087523f3a24a0684430c964403e46d
jaehong <newbie302@gmail.com>
2017-06-12 16:32:08 +0900

    납품업체와 통화한 기록이 없으면 내거래처 탭에서 가이드 레이아웃을 보여줌

--- app/src/main/java/com/rena21c/voiceorder/activities/MyPartnerActivity.java
+++ app/src/main/java/com/rena21c/voiceorder/activities/MyPartnerActivity.java
@@ -1,15 +1,35 @@
 package com.rena21c.voiceorder.activities;
 
 import android.os.Bundle;
+import android.view.View;
 
+import com.rena21c.voiceorder.App;
 import com.rena21c.voiceorder.R;
+import com.rena21c.voiceorder.etc.AppPreferenceManager;
+import com.rena21c.voiceorder.view.components.ReplaceableLayout;
 
 
 public class MyPartnerActivity extends HasTabActivity {
+
+    private AppPreferenceManager appPreferenceManager;
+
+    private ReplaceableLayout replaceableLayout;
+    private View myPartnerGuide;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_my_partner);
+
+        replaceableLayout = (ReplaceableLayout)findViewById(R.id.replaceableLayout);
+        myPartnerGuide = getLayoutInflater().inflate(R.layout.layout_component_my_partner_guide, replaceableLayout, false);
+
+        appPreferenceManager= App.getApplication(getApplicationContext()).getPreferenceManager();
+    }
+
+    @Override protected void onResume() {
+        super.onResume();
+        replaceableLayout.removeAllViews();
+        if(appPreferenceManager.getAllCallTime() == null) replaceableLayout.replaceChildView(myPartnerGuide);
     }
 }
