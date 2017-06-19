2f63d3272826499fda52cea8277342d54e1f0814
jaehong <newbie302@gmail.com>
2017-06-19 12:42:58 +0900

    Rename

--- app/src/main/java/com/rena21c/voiceorder/viewmodel/MyPartnerGuideViewModel.java
+++ app/src/main/java/com/rena21c/voiceorder/viewmodel/MyPartnerGuideViewModel.java
@@ -11,7 +11,7 @@ import com.rena21c.voiceorder.view.widgets.AddPartnerButton;
 public class MyPartnerGuideViewModel {
 
     public View getView(Context context) {
-        View view = LayoutInflater.from(context).inflate(R.layout.layout_component_my_partner_guide, null);
+        View view = LayoutInflater.from(context).inflate(R.layout.layout_my_partner_guide, null);
         AddPartnerButton addPartnerButton = (AddPartnerButton) view.findViewById(R.id.btnAddPartner);
 
         addPartnerButton.setAddPartnerListener((AddPartnerButton.AddPartnerListener)context);
