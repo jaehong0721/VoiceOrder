041a8f52da7b3c115fd509d80dbb42ae6266fbea
jaehong <newbie302@gmail.com>
2017-07-13 15:25:19 +0900

    RequestEstimateActivity(견적요청) 추가

---
+++ app/src/main/java/com/rena21c/voiceorder/activities/RequestEstimateActivity.java
@@ -0,0 +1,14 @@
+package com.rena21c.voiceorder.activities;
+
+import android.os.Bundle;
+
+import com.rena21c.voiceorder.R;
+
+public class RequestEstimateActivity extends HasTabActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_request_estimate);
+    }
+}
