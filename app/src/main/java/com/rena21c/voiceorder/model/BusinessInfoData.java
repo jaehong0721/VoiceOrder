370fd526eb1408aec7fc48db4690e9f234161a6b
jaehong <newbie302@gmail.com>
2017-08-16 01:25:39 +0900

    BusinessInfo class 추가

---
+++ app/src/main/java/com/rena21c/voiceorder/model/BusinessInfoData.java
@@ -0,0 +1,15 @@
+package com.rena21c.voiceorder.model;
+
+
+import java.util.List;
+
+public class BusinessInfoData {
+    public int partnerNum;
+    public int orderNum;
+    public int age;
+    public String closedDay;
+    public String deliveryTime;
+    public String businessLicenseNumber;
+    public List<String> deliveryAreas;
+    public List<String> majorItems;
+}
