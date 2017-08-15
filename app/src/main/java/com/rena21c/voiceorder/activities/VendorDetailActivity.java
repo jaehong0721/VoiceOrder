f6c1d885becc8cd5e7a7d4c17cb0b7878401d667
jaehong <newbie302@gmail.com>
2017-08-16 00:53:04 +0900

    VendorDetailActivity에 액션바 추가

--- app/src/main/java/com/rena21c/voiceorder/activities/VendorDetailActivity.java
+++ app/src/main/java/com/rena21c/voiceorder/activities/VendorDetailActivity.java
@@ -2,9 +2,11 @@ package com.rena21c.voiceorder.activities;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 
 import com.rena21c.voiceorder.R;
 import com.rena21c.voiceorder.util.StringUtil;
+import com.rena21c.voiceorder.view.actionbar.NavigateBackActionBar;
 
 
 public class VendorDetailActivity extends AppCompatActivity {
@@ -13,6 +15,15 @@ public class VendorDetailActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_vendor_detail);
+
+        NavigateBackActionBar.createWithActionBar(this, getSupportActionBar())
+                .setBackButtonClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        finish();
+                    }
+                })
+                .setTitle("업체상세");
 
         String phoneNumber = getIntent().getStringExtra("vendorPhoneNumber");
         phoneNumber = StringUtil.removeSpecialLetter(phoneNumber);
