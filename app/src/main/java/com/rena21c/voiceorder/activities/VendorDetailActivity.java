4eddc2069d1940e2265d9f3864a0d6a63cce05df
jaehong <newbie302@gmail.com>
2017-08-16 17:16:00 +0900

    이미지를 파이어베이스에서 s3에서 불러오는 것으로 수정

--- app/src/main/java/com/rena21c/voiceorder/activities/VendorDetailActivity.java
+++ app/src/main/java/com/rena21c/voiceorder/activities/VendorDetailActivity.java
@@ -2,17 +2,21 @@ package com.rena21c.voiceorder.activities;
 
 import android.os.Build;
 import android.os.Bundle;
+import android.os.Handler;
 import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.ImageView;
 
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.model.S3ObjectSummary;
 import com.google.firebase.database.DataSnapshot;
 import com.rena21c.voiceorder.App;
 import com.rena21c.voiceorder.R;
 import com.rena21c.voiceorder.firebase.FirebaseDbManager;
 import com.rena21c.voiceorder.firebase.ToastErrorHandlingListener;
+import com.rena21c.voiceorder.network.FileTransferUtil;
 import com.rena21c.voiceorder.util.StringUtil;
 import com.rena21c.voiceorder.view.actionbar.NavigateBackActionBar;
 import com.rena21c.voiceorder.view.adapters.VendorImageAdapter;
@@ -20,6 +24,7 @@ import com.rena21c.voiceorder.view.components.BusinessInfoContainer;
 import com.rena21c.voiceorder.view.components.ContactInfoContainer;
 import com.rena21c.voiceorder.view.components.ViewPagerWithIndicator;
 
+import java.util.ArrayList;
 import java.util.List;
 
 
@@ -31,6 +36,10 @@ public class VendorDetailActivity extends AppCompatActivity {
     private FirebaseDbManager dbManager;
 
     private String phoneNumber;
+
+    private AmazonS3 s3;
+    private String s3BucketName;
+    private String s3Address;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -45,6 +54,10 @@ public class VendorDetailActivity extends AppCompatActivity {
                     }
                 })
                 .setTitle("업체상세");
+
+        s3 = FileTransferUtil.getS3Client(this);
+        s3BucketName = getResources().getString(R.string.s3_image_bucket_name);
+        s3Address = getResources().getString(R.string.s3_base_address) + "/" + s3BucketName;
 
         phoneNumber = StringUtil.removeSpecialLetter(getIntent().getStringExtra("vendorPhoneNumber"));
 
@@ -61,15 +74,8 @@ public class VendorDetailActivity extends AppCompatActivity {
 
     @Override protected void onStart() {
         super.onStart();
-        dbManager.getVendorImages(phoneNumber, new ToastErrorHandlingListener(this) {
-            @Override public void onDataChange(DataSnapshot dataSnapshot) {
-                if(!dataSnapshot.exists()) return;
-                VendorImageAdapter vendorImageAdapter = new VendorImageAdapter(LayoutInflater.from(VendorDetailActivity.this));
-                imageViewPager.setAdapter(vendorImageAdapter);
-                vendorImageAdapter.setVendorImages((List)dataSnapshot.getValue());
-                imageViewPager.setVisibility(View.VISIBLE);
-            }
-        });
+
+        setVendorImage();
 
         dbManager.getVendorContactInfo(phoneNumber, new ToastErrorHandlingListener(this) {
             @Override public void onDataChange(DataSnapshot dataSnapshot) {
@@ -93,5 +99,27 @@ public class VendorDetailActivity extends AppCompatActivity {
                 dbManager.setVisitCount(phoneNumber, visitCount + 1);
             }
         });
+    }
+
+    private void setVendorImage() {
+        final Handler handler = new Handler(getMainLooper());
+        final List<String> imageUrls = new ArrayList<>();
+        new Thread(new Runnable() {
+            @Override public void run() {
+                for(final S3ObjectSummary file : s3.listObjects(s3BucketName, "image/" + phoneNumber + "/").getObjectSummaries()) {
+                    if(file.getKey().equals("image/" + phoneNumber)) continue;
+                    imageUrls.add(s3Address + "/" + file.getKey());
+                }
+                if(imageUrls.size() == 0) return;
+                handler.post(new Runnable() {
+                    @Override public void run() {
+                        VendorImageAdapter vendorImageAdapter = new VendorImageAdapter(LayoutInflater.from(VendorDetailActivity.this));
+                        imageViewPager.setAdapter(vendorImageAdapter);
+                        vendorImageAdapter.setVendorImages(imageUrls);
+                        imageViewPager.setVisibility(View.VISIBLE);
+                    }
+                });
+            }
+        }).start();
     }
 }
