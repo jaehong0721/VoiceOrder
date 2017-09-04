2afb359d1118c8847f9fabd1ae09a47600402d33
jaehong <newbie302@gmail.com>
2017-09-04 12:09:31 +0900

    견적 요청 품목 입력하기 추가

--- app/src/main/java/com/rena21c/voiceorder/activities/InputEstimateActivity.java
+++ app/src/main/java/com/rena21c/voiceorder/activities/InputEstimateActivity.java
@@ -3,12 +3,18 @@ package com.rena21c.voiceorder.activities;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.Button;
+import android.widget.LinearLayout;
 
 import com.rena21c.voiceorder.R;
 import com.rena21c.voiceorder.view.actionbar.CloseActivityActionBar;
+import com.rena21c.voiceorder.view.widgets.EstimateInputView;
 
 
 public class InputEstimateActivity extends AppCompatActivity {
+
+    private Button btnAddEstimateInputView;
+    private LinearLayout estimateInputViewContainer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,5 +29,14 @@ public class InputEstimateActivity extends AppCompatActivity {
                     }
                 })
                 .setTitle("견적요청");
+
+        estimateInputViewContainer = (LinearLayout) findViewById(R.id.estimate_input_view_container);
+
+        btnAddEstimateInputView = (Button) findViewById(R.id.btnAddEstimateInputView);
+        btnAddEstimateInputView.setOnClickListener(new View.OnClickListener() {
+            @Override public void onClick(View v) {
+                estimateInputViewContainer.addView(new EstimateInputView(InputEstimateActivity.this));
+            }
+        });
     }
 }
